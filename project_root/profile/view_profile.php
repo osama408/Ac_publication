7e2a3ea128c91d5ce33f5bd6c1f063580fae29e3
osama408 <ug631431024@ftu.ac.th>
2025-01-05 00:42:31 -0800

    Update view_profile.php

--- project_root/profile/view_profile.php
+++ project_root/profile/view_profile.php
@@ -1,12 +1,6 @@
 <?php
-require '../../includes/db_connect.php';
 session_start();
-
-// hide errors
-ini_set('display_errors','Off');
-ini_set('error_reporting', E_ALL );
-define('WP_DEBUG', false);
-define('WP_DEBUG_DISPLAY', false);
+require '../../includes/db_connect.php';
 
 // Check if user is authenticated
 if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
