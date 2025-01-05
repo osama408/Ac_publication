6f92c209632b4843d5386e6059e57c8c8663d5ca
osama408 <ug631431024@ftu.ac.th>
2025-01-05 08:14:17 -0800

    Update dashboard.php

--- project_root/dashboard.php
+++ project_root/dashboard.php
@@ -41,7 +41,7 @@ if(isset($_POST['publish']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lara Admin 0.1</title>
+    <title>Admin Panel</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
    
