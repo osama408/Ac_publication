90379098b576dd5becd0cc7902d6cb9cc8d3a8ed
osama408 <ug631431024@ftu.ac.th>
2025-01-05 00:32:58 -0800

    Update update_essay.php

--- project_root/Essays/update_essay.php
+++ project_root/Essays/update_essay.php
@@ -1,18 +1,15 @@
 <?php 
-// Hide errors
-ini_set('display_errors', 'Off');
-ini_set('error_reporting', E_ALL);
-define('WP_DEBUG', false);
-define('WP_DEBUG_DISPLAY', false);
-require '../../includes/db_connect.php';
-
 session_start();
+require '../../includes/db_connect.php';
 
 if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
     // Redirect to login page if not authenticated
     header("location: ../login.php");
     exit;
 }
+
+
+
 if (isset($_GET['update_id'])) {
     $edit = mysqli_real_escape_string($connect, $_GET['update_id']);
     
